ed0cfeb96d05b04c002614099f7204290353eeb3
Firedaisi <xainabdaisi@gmail.com>
2024-04-14 18:52:15 +0100

    14.4.24

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
-var a;
-a=10;
-b=11;
-var c = 12;
-console.log("a= "+ a +"b= "+b + "c= " +c)
+//import logger from './tools.js'
+//logger("Welcome! We are now having fun with modularity");
+
+//import logger, {appName, dummyFunction} from './tools.js';
+//logger("Welcome! The application name is " + appName + ". There is a function that returns " + dummyFunction() );
+
+//import logger, {appName, dummyFunction} from './tools.js';
+//logger(`Welcome! The application name is "${appName}". There is
+//a function that returns "${dummyFunction()}".`);
+
+//import logger, {appName, dummyFunction} from './tools.js';
+//logger(`Welcome! The application name is "${appName}".
+//There is a function that returns "${dummyFunction()}".`);
+
+//import logger, {appName, dummyFunction}
+//from './tools.js'
+//logger(`Welcome! The application name is "${appName}". There is a function
+//that returns "${dummyFunction()}".` );
+
+// index.js
+
+import logger from './tools.js'; // Assuming default export in tools.js
+const addressees = [
+  "John Uzo”", // Double-check the extra quotation mark here
+  "Mary Smart",
+  "Paul Umoh"
+];
+addressees.map(addressee => {
+  let message = `Dear ${addressee},
+It is my pleasure to inform you that your admission letter is ready for collection.
+Sincerely,
+The Admissions Team`; // Notice the proper newline characters
+  logger(message);
+});
+
---
+++ src/tools.js
@@ -0,0 +1,49 @@
+// var logger = function(output){
+  //  console.log(output)
+  //  }
+  //  export default logger; 
+
+  //  var logger = function(output){
+    //    console.log(output)
+      //  }
+        //export var appName = "ES6 Review";
+        //export var dummyFunction = function(){
+       // return "I am a dummy function";
+        //}
+       // export default logger; 
+
+    //   var logger = function(output){
+      //  console.log(output);
+        //}
+      //  export var appName = "ES6 Review";
+        //export var dummyFunction = function(){
+        //return "I am a dummy function";
+        //}
+        //export default logger;
+        
+     //   var logger = output => {
+       //     console.log(output)
+         //   }
+           // export var appName = "ES6 Review";
+            //export var dummyFunction = () => {
+           // return "I am a dummy function";
+            //}
+            //export default logger; 
+  
+//const logger = output => {
+//console.log(output)
+//}
+//export const appName = "ES6 Review";
+//export const dummyFunction = () => {
+//let feedback = "I am a dummy function"; //mutable variable
+//feedback = "I am still a dummy function";//value changed
+//return feedback;
+//}
+//export default logger; 
+
+// tools.js
+
+export default function logger(message) {
+    console.log(message);
+  }
+  
